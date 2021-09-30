97aaa41104784411512cfcf4906b5fee46a9be02
Srivatsan T <CS20BTECH11062@iith.ac.in>
2021-09-30 19:37:20 +0530

    BST insert and delete

--- Assignment-2/Assignment_2.c
+++ Assignment-2/Assignment_2.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 struct song_node
 {
-    char* name;
+    char *name;
     struct song_node *parent;
     struct song_node *left;
     struct song_node *right;
 };
 
-struct song_node* root = NULL;
+struct song_node *root = NULL;
 
-void song_node_constructor(struct song_node a,char* n, struct song_node *p, struct song_node *l, struct song_node *r)
+void song_node_constructor(struct song_node a, char *n, struct song_node *p, struct song_node *l, struct song_node *r)
 {
     a.name = n;
     a.parent = p;
@@ -21,9 +21,85 @@ void song_node_constructor(struct song_node a,char* n, struct song_node *p, stru
     a.right = r;
 }
 
-void insert_bst(struct song_node a)
+int is_root(struct song_node *a)
 {
-    
+    if (a->parent == NULL)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int is_leaf(struct song_node *a)
+{
+    if (a->left == NULL && a->right == NULL)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+void traverse_bst(struct song_node *a)
+{
+    struct song_node *temp = a;
+    while (is_leaf(temp) != 0)
+    {
+        traverse_bst(temp->left);
+        printf("%s : ", temp->name);
+        traverse_bst(temp->right);
+    }
+
+    printf("%s : ", temp->name);
+}
+
+void insert_bst(struct song_node *a)
+{
+    struct song_node *temp = a;
+    while (is_leaf(temp) != 0)
+    {
+        if (strcmpi(temp, a) < 0)
+        {
+            temp = temp->right;
+        }
+        else
+        {
+            temp = temp->left;
+        }
+    }
+    if (strcmpi(temp, a) < 0)
+    {
+        temp->right = a;
+        temp->left = NULL;
+    }
+    else
+    {
+        temp->left = a;
+        temp->right = NULL;
+    }
+}
+
+void delete_bst(struct song_node *a)
+{
+    struct song_node *temp = a;
+    while (is_leaf(temp) != 0)
+    {
+        if (strcmpi(temp, a) < 0)
+        {
+            temp = temp->right;
+        }
+        else
+        {
+            temp = temp->left;
+        }
+    }
+    if (strcmpi(temp, a) < 0)
+    {
+        temp->right = NULL;
+    }
+    else
+    {
+        temp->left = NULL;
+    }
 }
 
 
