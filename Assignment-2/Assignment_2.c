3dbd04c8ed08c3b9f1adde46d8d56bd0fce2663f
Srivatsan T <CS20BTECH11062@iith.ac.in>
2021-09-30 19:07:21 +0530

    started BST

--- Assignment-2/Assignment_2.c
+++ Assignment-2/Assignment_2.c
@@ -3,9 +3,33 @@
 #include <stdlib.h>
 
 
+struct song_node
+{
+    char* name;
+    struct song_node *parent;
+    struct song_node *left;
+    struct song_node *right;
+};
+
+struct song_node* root = NULL;
+
+void song_node_constructor(struct song_node a,char* n, struct song_node *p, struct song_node *l, struct song_node *r)
+{
+    a.name = n;
+    a.parent = p;
+    a.left = l;
+    a.right = r;
+}
+
+void insert_bst(struct song_node a)
+{
+    
+}
+
+
 char *read_from_file(char *file_name)
 {
-    char *file_content = malloc(100*sizeof(char));
+    char *file_content = malloc(100 * sizeof(char));
     FILE *fptr;
     fptr = fopen(file_name, "r");
     if (fptr != NULL)
@@ -16,9 +40,8 @@ char *read_from_file(char *file_name)
     return file_content;
 }
 
-
 int main()
 {
-    printf("%s",read_from_file("songs.txt"));
+    printf("%s", read_from_file("songs.txt"));
     return EXIT_SUCCESS;
 }
