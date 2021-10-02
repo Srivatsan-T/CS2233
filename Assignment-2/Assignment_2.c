d3bd439d583526aa70c3f9da442764e1671ce10b
Srivatsan T <CS20BTECH11062@iith.ac.in>
2021-10-02 09:23:34 +0530

    implemented deletion for bst

--- Assignment-2/Assignment_2.c
+++ Assignment-2/Assignment_2.c
@@ -47,6 +47,30 @@ int is_leaf(struct song_node *a)
     return 0;
 }
 
+int number_children(struct song_node *a)
+{
+    int count = 0;
+    if (a->left != NULL)
+    {
+        count++;
+    }
+    if (a->right != NULL)
+    {
+        count++;
+    }
+    return count;
+}
+
+struct song_node * find_lowest(struct song_node *a)
+{
+    struct song_node *temp = a;
+    while(temp->left != NULL)
+    {
+        temp = temp->left;
+    }
+    return temp;
+}
+
 void traverse_bst(struct song_node *a)
 {
     struct song_node *temp = a;
@@ -100,10 +124,65 @@ void insert_bst(struct song_node *a)
     }
 }
 
-void delete_bst(struct song_node *a)
+void delete_bst(char* n)
 {
-    struct song_node *temp = a;
-    
+    struct song_node *temp = root;
+    struct song_node *temp2 = root;
+
+    while (temp->name != n)
+    {
+        temp2 = temp;
+        if (strcmpi(temp->name, n) < 0)
+        {
+            temp = temp->right;
+        }
+        else
+        {
+            temp = temp->left;
+        }
+    }
+    if (number_children(temp) == 0)
+    {
+        if (strcmpi(temp2->name, n) < 0)
+        {
+            temp2->right = NULL;
+        }
+        else
+        {
+            temp2->left = NULL;
+        }
+    }
+
+    else if (number_children(temp) == 1)
+    {
+        struct song_node *child = NULL;
+        if (temp->right != NULL)
+        {
+            child = temp->right;
+        }
+        else
+        {
+            child = temp->left;
+        }
+
+        if (strcmpi(temp2->name, n) < 0)
+        {
+            temp2->right = child;
+        }
+        else
+        {
+            temp2->left = child;
+        }
+    }
+
+    else if(number_children(temp) == 2)
+    {
+        char* temp_name;
+        struct song_node *succ = find_lowest(temp->right);
+        temp_name = succ->name;
+        delete_bst(succ->name);
+        temp->name = temp_name;
+    }
 }
 
 char *read_from_file(char *file_name)
@@ -133,12 +212,23 @@ void insertion_set()
     struct song_node d;
     song_node_name_init(&d, "Despacito");
     insert_bst(&d);
+    struct song_node e;
+    song_node_name_init(&e, "Camila");
+    insert_bst(&e);
     traverse_bst(root);
 }
 
+void deletion_set()
+{
+    delete_bst("Despacito");
+    traverse_bst(root);
+    delete_bst("Closer");
+    traverse_bst(root);
+}
 int main()
 {
     printf("%s", read_from_file("songs.txt"));
     insertion_set();
+    deletion_set();
     return EXIT_SUCCESS;
 }
