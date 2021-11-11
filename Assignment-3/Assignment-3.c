ff36fa89d34cd32abfe4a0badcf7367b721fcf30
Srivatsan T <CS20BTECH11062@iith.ac.in>
2021-11-11 21:42:27 +0530

    bst is complete...have to work on btrees now

--- Assignment-3/Assignment-3.c
+++ Assignment-3/Assignment-3.c
@@ -5,10 +5,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-// Initialising primary and secondary memories
-cell_node primary_memory[t * 2];
-cell_node secondary_memory[MAX_ROWS][t * 2];
+#include <string.h>
 
 struct addressing_nodes_
 {
@@ -20,17 +17,22 @@ typedef struct addressing_nodes_ node_address;
 
 struct cell_node_
 {
-    int does_left_exist;
+    node_address left_node;
     int key;
+    node_address parent_node;
 };
 
 typedef struct cell_node_ cell_node;
+
+// Initialising primary and secondary memories
+cell_node primary_memory[t * 2];
+cell_node secondary_memory[MAX_ROWS][t * 2];
 
 void readDisk(int i)
 {
     for (unsigned j = 0; j < 2 * t; j++)
     {
-        primary_memory[i] = secondary_memory[i][j];
+        primary_memory[j] = secondary_memory[i][j];
     }
 }
 
@@ -38,70 +40,180 @@ void writeDisk(int i)
 {
     for (unsigned j = 0; j < 2 * t; j++)
     {
-        secondary_memory[i][j] = primary_memory[i];
+        secondary_memory[i][j] = primary_memory[j];
     }
 }
 
-void traverse_bst()
+/*
+void traverse_bst(int str_row)
 {
-    
+    readDisk(str_row);
+    if (primary_memory[0].left_node.row_number == -1 && primary_memory[1].left_node.row_number == -1)
+    {
+        printf("%d : ", primary_memory[0].key);
+    }
+    else if (primary_memory[0].left_node.row_number == -1)
+    {
+        printf("%d : ", primary_memory[0].key);
+        traverse_bst(primary_memory[1].left_node.row_number);
+    }
+    else if (primary_memory[1].left_node.row_number == -1)
+    {
+        traverse_bst(primary_memory[0].left_node.row_number);
+        printf("%d : ", primary_memory[0].key);
+    }
+    else
+    {
+        traverse_bst(primary_memory[0].left_node.row_number);
+        printf("%d : ", primary_memory[0].key);
+        traverse_bst(primary_memory[1].left_node.row_number);
+    }
 }
+*/
 
-void search_bst(int find_key)
+int search_bst(int find_key)
 {
     readDisk(0);
     int pres_row_num = 0;
     while (primary_memory[0].key != find_key)
     {
-        if (primary_memory[0].key < find_key && primary_memory[0].left != NO)
+        if (primary_memory[0].key < find_key && primary_memory[1].left_node.row_number != -1)
         {
-            pres_row_num = primary_memory[0].left.row_number;
+            pres_row_num = primary_memory[1].left_node.row_number;
             readDisk(pres_row_num);
         }
-        else if (primary_memory[0].key > find_key && primary_memory[1].left != NO)
+        else if (primary_memory[0].key > find_key && primary_memory[0].left_node.row_number != -1)
         {
-            pres_row_num = primary_memory[1].left.row_number;
+            pres_row_num = primary_memory[0].left_node.row_number;
             readDisk(pres_row_num);
         }
     }
+    return pres_row_num;
 }
 
-void insert_bst(int ins_key)
+void insert_bst(int ins_key, int row_num)
 {
     readDisk(0);
     int pres_row_num = 0;
-    while (primary_memory[0].left != NO || primary_memory[1].left != NO)
+    while (primary_memory[0].left_node.row_number != -1 || primary_memory[1].left_node.row_number != -1)
     {
-        if (primary_memory[0].key < ins_key && primary_memory[0].left != NO)
+        if (primary_memory[0].key < ins_key)
         {
-            pres_row_num = primary_memory[0].left.row_number;
-            readDisk(pres_row_num);
+            if (primary_memory[1].left_node.row_number != -1)
+            {
+                pres_row_num = primary_memory[1].left_node.row_number;
+                readDisk(pres_row_num);
+            }
+            else
+            {
+                break;
+            }
         }
-        else if (primary_memory[0].key > ins_key && primary_memory[1].left != NO)
+        else if (primary_memory[0].key > ins_key)
         {
-            pres_row_num = primary_memory[1].left.row_number;
-            readDisk(pres_row_num);
+            if (primary_memory[0].left_node.row_number != -1)
+            {
+                pres_row_num = primary_memory[0].left_node.row_number;
+                readDisk(pres_row_num);
+            }
+            else
+            {
+                break;
+            }
         }
     }
-    if (primary_memory[0].key < ins_key)
+    if (primary_memory[0].key == -1)
     {
-        primary_memory[0].left = YES;
-        writeDisk(pres_row_num);
-        readDisk(2 * pres_row_num);
         primary_memory[0].key = ins_key;
-        primary_memory[0].left = NO;
-        primary_memory[1].left = NO;
-        writeDisk(2 * pres_row_num);
+        primary_memory[0].parent_node.row_number = primary_memory[1].parent_node.row_number = -1;
+        writeDisk(pres_row_num);
+    }
+    else if (primary_memory[0].key < ins_key)
+    {
+        primary_memory[1].left_node.row_number = row_num;
+        writeDisk(pres_row_num);
+        readDisk(row_num);
+        primary_memory[0].key = ins_key;
+        primary_memory[0].left_node.row_number = primary_memory[1].left_node.row_number = -1;
+        primary_memory[0].parent_node.row_number = primary_memory[1].parent_node.row_number = pres_row_num;
+        writeDisk(row_num);
     }
     else
     {
-        primary_memory[1].left = YES;
+        primary_memory[0].left_node.row_number = row_num;
         writeDisk(pres_row_num);
-        readDisk(2 * pres_row_num + 1);
+        readDisk(row_num);
         primary_memory[0].key = ins_key;
-        primary_memory[0].left = NO;
-        primary_memory[1].left = NO;
-        writeDisk(2 * pres_row_num + 1);
+        primary_memory[0].left_node.row_number = primary_memory[1].left_node.row_number = -1;
+        primary_memory[0].parent_node.row_number = primary_memory[1].parent_node.row_number = pres_row_num;
+        writeDisk(row_num);
+    }
+}
+
+int find_successor(int find_key)
+{
+    int succ_row;
+    succ_row = primary_memory[1].left_node.row_number;
+    readDisk(succ_row);
+    while (primary_memory[0].left_node.row_number != -1)
+    {
+        succ_row = primary_memory[0].left_node.row_number;
+        readDisk(succ_row);
+    }
+    return succ_row;
+}
+
+void delete_bst(int del_key)
+{
+    int row = search_bst(del_key);
+    readDisk(row);
+    if (primary_memory[0].left_node.row_number == -1 && primary_memory[1].left_node.row_number == -1)
+    {
+        int parent_row = primary_memory[0].parent_node.row_number;
+        readDisk(parent_row);
+        if (primary_memory[0].left_node.row_number == row)
+        {
+            primary_memory[0].left_node.row_number = -1;
+        }
+        else if (primary_memory[1].left_node.row_number == row)
+        {
+            primary_memory[1].left_node.row_number = -1;
+        }
+        writeDisk(parent_row);
+        readDisk(row);
+        primary_memory[0].key = -1;
+        primary_memory[0].left_node.row_number = primary_memory[1].left_node.row_number = -1;
+        primary_memory[0].parent_node.row_number = primary_memory[1].parent_node.row_number = -1;
+        writeDisk(row);
+    }
+    else if (primary_memory[0].left_node.row_number == -1 || primary_memory[1].left_node.row_number == -1)
+    {
+        int child_row;
+        if (primary_memory[0].left_node.row_number == -1)
+        {
+            child_row = primary_memory[1].left_node.row_number;
+        }
+        else if (primary_memory[1].left_node.row_number == -1)
+        {
+            child_row = primary_memory[0].left_node.row_number;
+        }
+        readDisk(child_row);
+        int child_key = primary_memory[0].key;
+        primary_memory[0].key = -1;
+        writeDisk(child_row);
+        readDisk(row);
+        primary_memory[0].key = child_key;
+        writeDisk(row);
+    }
+    else
+    {
+        int succ_row = find_successor(del_key);
+        readDisk(succ_row);
+        int succ_key = primary_memory[0].key;
+        delete_bst(succ_key);
+        readDisk(row);
+        primary_memory[0].key = succ_key;
+        writeDisk(row);
     }
 }
 
@@ -122,16 +234,38 @@ void file_to_function(char *songs_list)
 {
     char *token = strtok(songs_list, ":");
     int atoi_token;
+    int row_num = 0;
     while (token != NULL)
     {
         atoi_token = atoi(token);
         token = strtok(NULL, ":");
-        insert_bst(atoi_token);
+        insert_bst(atoi_token, row_num);
+        row_num++;
     }
 }
 
 int main()
 {
-
+    readDisk(0);
+    primary_memory[0].left_node.row_number = primary_memory[1].left_node.row_number = -1;
+    primary_memory[0].key = -1;
+    writeDisk(0);
+    file_to_function(read_from_file("keys.txt"));
+    /*
+    for (int i = 0; i < 60; i++)
+    {
+        readDisk(i);
+        printf("%d : ", primary_memory[0].key);
+    }
+    printf("\n");
+    */
+    delete_bst(17);
+    for (int i = 0; i < 10; i++)
+    {
+        readDisk(i);
+        printf("%d : ", primary_memory[0].key);
+    }
+    printf("\n");
+    // traverse_bst(0);
     return 0;
 }
